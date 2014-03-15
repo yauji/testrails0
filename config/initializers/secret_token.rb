a3a1ce3f8ada3f942064b2486cfc4db0ad4ecf64
Ubuntu <ubuntu@ip-10-166-5-205.ap-northeast-1.compute.internal>
2014-03-15 06:33:47 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testgithub0::Application.config.secret_token = 'b45cb7c5030837625f469735d9e8877bfa8dda5ed1b85d5a759b1290786045919e220bd7fdf9111a3cf237f407b8ff8a64965a194b92521d46537995b0eca0f6'
